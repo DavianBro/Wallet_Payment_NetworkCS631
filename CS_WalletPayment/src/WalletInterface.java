70985bfd4b8bc65043f4750f0203aa1f4f4efb60
DavianBro <74699056+DavianBro@users.noreply.github.com>
2021-12-06 15:36:59 -0500

    Davian's Update

--- CS_WalletPayment/src/WalletInterface.java
+++ CS_WalletPayment/src/WalletInterface.java
@@ -1,5 +1,24 @@
-public class WalletInterface {
 
-    
+
+import java.util.*;
+
+public class  WalletInterface {
+
+    public static void main(String[]args)   {
+
+        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.printf("\n");
+        System.out.print("============= WELCOME TO THE WALLET PAYMENT NETWORK =============");
+        System.out.printf("\n");
+        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.printf("\n");
+        System.out.printf("\n");
+
+
+
+    }
+
 
 }
