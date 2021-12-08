77dd697992eee713e1be4fa22b572eb505f52741
Ahamed Arafaath Ali <75636961+ahmed-arafaath@users.noreply.github.com>
2021-12-07 23:33:53 -0500

    Ahmed's update

--- CS_WalletPayment/src/WalletInterface.java
+++ CS_WalletPayment/src/WalletInterface.java
@@ -3,58 +3,84 @@
 import java.util.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.sql.*;
 
 public class  WalletInterface extends Wallet_Methods {
 
-    public static void main(String[]args)   {
-           // Declare Time and Date 
-        LocalDateTime now = LocalDateTime.now();
-        // Format Date and Time
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        //Alter the object now to format
-        String formatDateTime = now.format(format);
-        // Declare Scanner
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[]args) throws Exception {
 
-        System.out.println(formatDateTime);
-        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
-        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
-        System.out.printf("\n");
-        System.out.print("============= WELCOME TO THE WALLET PAYMENT NETWORK =============");
-        System.out.printf("\n");
-        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
-        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
-        System.out.printf("\n");
-        System.out.printf("\n");
+        //////////////////////////////////////////////////////////////////
+        // to be customized according to your own devices
+        String url = "jdbc:mysql://localhost:3306/WalletNetwork";
+        String uname = "root"; //username I have set in my device, change to whatever you have set in your device
+        String password = "Wallet_network631"; //password I have set in my device, change to whatever your password is
+        ///////////////////////////////////////////////////////////////////
+
+        //setting up connection with local database
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, uname, password);
 
 
+            // Declare Time and Date
+            LocalDateTime now = LocalDateTime.now();
+            // Format Date and Time
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+            //Alter the object now to format
+            String formatDateTime = now.format(format);
+            // Declare Scanner
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter 1 to Verify That You're Not A Robot");
-        String Robot_verify = sc.nextLine();
+            System.out.println(formatDateTime);
+            System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+            System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+            System.out.printf("\n");
+            System.out.print("============= WELCOME TO THE WALLET PAYMENT NETWORK =============");
+            System.out.printf("\n");
+            System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+            System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+            System.out.printf("\n");
+            System.out.printf("\n");
 
 
-        if(Robot_verify.equals("1")){
+            System.out.println("Enter 1 to Verify That You're Not A Robot");
+            String Robot_verify = sc.nextLine();
 
-            System.out.println(" 1.) Press 1 to Login");
-            // If equals 1 declare method in Wallets_Methods
-            System.out.println(" 2.) Press 2 to Create Account");
+            if(Robot_verify.equals("1")){
 
-// Call Method
+                System.out.println(" 1.) Press 1 to Login");
+                System.out.println(" 2.) Press 2 to Create Account");
 
+            }else{
 
-
-        }else{
-
-            System.out.println(" Access Denied to Wallet Payment Network. Please Try Again");
+                System.out.println(" Access Denied to Wallet Payment Network. Please Try Again");
 
 
+            }
+
+            Scanner login = new Scanner(System.in);
+            System.out.print("Please enter your email: ");
+            String login_verify = login.next();
+
+            Statement st = con.createStatement();
+            ResultSet result = st.executeQuery("select EmailAdd from EMAIL;");
+
+            while (result.next()) {
+                String email_verify = result.getString(1);
+                if (login_verify.equals(email_verify)) {
+                    System.out.println("Thanks for logging in, welcome back");
+                }
+
+            }
+
+
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
 
-
-
-
-      /*
+        /*
       * Once Logged in
       * Account info
       * Send money
@@ -68,6 +94,5 @@ public class  WalletInterface extends Wallet_Methods {
 
 
     }
-
 
 }
