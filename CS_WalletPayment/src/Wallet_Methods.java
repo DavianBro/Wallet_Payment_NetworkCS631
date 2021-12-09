1e01168441b057d7534f5c152cb3d11743088f9c
DavianBro <74699056+DavianBro@users.noreply.github.com>
2021-12-09 13:15:52 -0500

    Update Wallet_Methods.java

--- CS_WalletPayment/src/Wallet_Methods.java
+++ CS_WalletPayment/src/Wallet_Methods.java
@@ -16,10 +16,10 @@ public class Wallet_Methods {
             if (Login_Password.equals("0631")){ // and equals an email in the database
                 System.out.println();
                 System.out.println();
-                System.out.println("Welcome Back User Here Are Your Options: "); // Change User to name in Databse
+                System.out.println("Welcome Back " + " WALLER USER INSERT VARIABLE HERE" + " Here Are Your Options: "); // Change User to name in Databse
                 System.out.println(" --------------------------------------------");
 
-                
+
 
 
             }
