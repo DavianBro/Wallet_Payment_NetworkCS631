bc6883c152052d08cf4b0b1a9bccd3c6446ca8db
DavianBro <74699056+DavianBro@users.noreply.github.com>
2021-12-09 13:24:18 -0500

    Update Wallet_Methods.java

--- CS_WalletPayment/src/Wallet_Methods.java
+++ CS_WalletPayment/src/Wallet_Methods.java
@@ -9,6 +9,7 @@ public class Wallet_Methods {
     if(x.equals("1")) {
     System.out.println("Enter Email Address");
     String Email_Address = sc.nextLine();
+    // If Login Equals Email in db then prompt user for password
     // Initialize sql to Email Address and check if its in the data base
         System.out.println("Enter Password");
         String Login_Password = sc.nextLine();
@@ -19,7 +20,19 @@ public class Wallet_Methods {
                 System.out.println("Welcome Back " + " WALLER USER INSERT VARIABLE HERE" + " Here Are Your Options: "); // Change User to name in Databse
                 System.out.println(" --------------------------------------------");
 
+                System.out.println();
+                //System.out.printf
+                String Withdraw = "1. Withdraw";
+                String Deposit = "2. Deposit";
+                String TransferFunds = "3.Transfer Funds";
+                String CheckBalance = "4.Check Balance";
+                String Exit = "5. Exit";
 
+                Scanner reader = new Scanner(System.in);
+                System.out.printf("%15s %15s %20s\n", Withdraw, Deposit, TransferFunds );
+                System.out.printf("\n");
+                System.out.printf("\t%18s%18s\n", CheckBalance, Exit);
+                int optionmenu = reader.nextInt();
 
 
             }
