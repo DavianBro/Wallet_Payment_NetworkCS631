ece0c4f30fc156fa7c8da8eaa07e902c8343bc5a
DavianBro <74699056+DavianBro@users.noreply.github.com>
2021-12-11 17:24:02 -0500

    Davian's Update

--- CS_WalletPayment/src/Wallet_Methods.java
+++ CS_WalletPayment/src/Wallet_Methods.java
@@ -8,35 +8,8 @@ public class Wallet_Methods {
 
     static Scanner sc = new Scanner(System.in);
 
-    // Initialize DB Variables
-    static String url = "jdbc:mysql://localhost:3306/WalletNetwork";
-    static  String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static String uname = "root"; //username I have set in my device, change to whatever you have set in your device
-    static  String password = "Wallet_network631";
 
-
-    // Method to Connect to SQL Database
-    public static Connection sql_connection() throws ClassNotFoundException, SQLException {
-
-
-
-        try (Connection conn = DriverManager.getConnection(url, uname, password)) {
-
-            if (conn != null) {
-                System.out.println("Connected to the database!");
-                return conn;
-            } else {
-                System.out.println("Failed to make connection!");
-            }
-
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
+    
 
     // Login Verification Method
     public static void login_verification (String x) {
---
+++ CS_WalletPayment/src/dbconnect.java
@@ -0,0 +1,41 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbconnect {
+
+    // Initialize DB Variables
+    static String url = "jdbc:mysql://localhost:3306/WalletNetwork";
+    static  String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static String uname = "root"; //username I have set in my device, change to whatever you have set in your device
+    static  String password = "Wallet_network631";
+
+
+
+
+    public static Connection connect () throws ClassNotFoundException, SQLException {
+
+        try (Connection conn = DriverManager.getConnection(url, uname, password)) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+                return conn;
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+
+    }
+
+
+
+
+}
